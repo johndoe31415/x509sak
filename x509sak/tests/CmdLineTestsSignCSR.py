356f5f803cc9884c7f634f93e34dbfa641d5ac35
Johannes Bauer <joe@johannes-bauer.com>
2018-08-01 00:01:58 +0200

    Added sign CSR tests for subject taking/override
    When signing a CSR, take the subject of the CSR into the CRT when no
    subject is specified. Otherwise, override the complete subject. I.e.,
    test there's no mixing/matching going on.

--- x509sak/tests/CmdLineTestsSignCSR.py
+++ x509sak/tests/CmdLineTestsSignCSR.py
@@ -32,19 +32,47 @@ class CmdLineTestsSignCSR(unittest.TestCase):
 
 	def test_sign_pubkey(self):
 		with tempfile.TemporaryDirectory() as tempdir, WorkDir(tempdir):
-			# Create a default CSR
-			SubprocessExecutor.run([ self._x509sak, "createcsr", "request1.key", "request1.csr" ])
-
 			# Creata a CA
 			SubprocessExecutor.run([ self._x509sak, "createca", "myCA" ])
 
+			# Create a default CSR
+			SubprocessExecutor.run([ self._x509sak, "createcsr", "client.key", "client.csr" ])
+
 			# Sign the CSR
-			SubprocessExecutor.run([ self._x509sak, "signcsr", "myCA", "request1.csr", "request1.crt" ])
+			SubprocessExecutor.run([ self._x509sak, "signcsr", "myCA", "client.csr", "client.crt" ])
 
 			# Extract public key from private key
-			(success, pubkey_in) = SubprocessExecutor.run([ "openssl", "ec", "-pubout", "-in", "request1.key" ], return_stdout = True, discard_stderr = True)
+			(success, pubkey_in) = SubprocessExecutor.run([ "openssl", "ec", "-pubout", "-in", "client.key" ], return_stdout = True, discard_stderr = True)
 
 			# Extract public key from certificate
-			(success, pubkey_out) = SubprocessExecutor.run([ "openssl", "x509", "-noout", "-pubkey", "-in", "request1.crt" ], return_stdout = True)
+			(success, pubkey_out) = SubprocessExecutor.run([ "openssl", "x509", "-noout", "-pubkey", "-in", "client.crt" ], return_stdout = True)
 
 			self.assertEqual(pubkey_in, pubkey_out)
+
+	def test_take_override_subject(self):
+		with tempfile.TemporaryDirectory() as tempdir, WorkDir(tempdir):
+			# Creata a CA
+			SubprocessExecutor.run([ self._x509sak, "createca", "myCA" ])
+
+			# Create a CSR with a specific CN/OU
+			SubprocessExecutor.run([ self._x509sak, "createcsr", "-s", "/CN=CSR_CN_Subject/OU=CSR_OU_Subject", "client.key", "client.csr" ])
+
+			# Sign the CSR and take all original subject data
+			SubprocessExecutor.run([ self._x509sak, "signcsr", "myCA", "client.csr", "client_original.crt" ])
+
+			# Sign the CSR and override all subject parameters
+			SubprocessExecutor.run([ self._x509sak, "signcsr", "-s", "/CN=Foobar", "myCA", "client.csr", "client_override.crt" ])
+
+			# Get text representation of CSR and CRT
+			(success, csr_text) = SubprocessExecutor.run([ "openssl", "req", "-noout", "-text", "-in", "client.csr" ], return_stdout = True)
+			(success, crt_orig_text) = SubprocessExecutor.run([ "openssl", "x509", "-noout", "-text", "-in", "client_original.crt" ], return_stdout = True)
+			(success, crt_override_text) = SubprocessExecutor.run([ "openssl", "x509", "-noout", "-text", "-in", "client_override.crt" ], return_stdout = True)
+
+			# The subject is in the CSR and the CRT without any specification,
+			# but not in the CRT where it was overriden
+			self.assertIn(b"CSR_CN_Subject", csr_text)
+			self.assertIn(b"CSR_OU_Subject", csr_text)
+			self.assertIn(b"CSR_CN_Subject", crt_orig_text)
+			self.assertIn(b"CSR_OU_Subject", crt_orig_text)
+			self.assertNotIn(b"CSR_CN_Subject", crt_override_text)
+			self.assertNotIn(b"CSR_OU_Subject", crt_override_text)
