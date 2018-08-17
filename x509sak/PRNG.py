060643ea638e1a2815a658185e76a0ea9520691f
Johannes Bauer <joe@johannes-bauer.com>
2018-08-17 23:51:20 +0200

    Implement PRNG for testing
    For testing the scraper we need to have consistent base data; implement
    the PRNG for this. SHA512 gives the greatest performance of the bunch.

---
+++ x509sak/PRNG.py
@@ -0,0 +1,73 @@
+#	x509sak - The X.509 Swiss Army Knife white-hat certificate toolkit
+#	Copyright (C) 2018-2018 Johannes Bauer
+#
+#	This file is part of x509sak.
+#
+#	x509sak is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	x509sak is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with x509sak; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <JohannesBauer@gmx.de>
+
+import time
+import hashlib
+
+class PRNG(object):
+	def __init__(self, seed = None):
+		self._seed = seed
+		self._buffer = bytearray()
+		self.reset()
+
+	def get(self, length):
+		self._fill_buffer(length)
+		(result, self._buffer) = (self._buffer[ : length], self._buffer[length : ])
+		return result
+
+	def _fill_buffer(self, length):
+		while len(self._buffer) < length:
+			self._buffer += self._next_chunk()
+
+	def reset(self):
+		self.init_state(self._seed or b"123456789")
+
+	def init_state(self, state):
+		raise NotImplementedError("init_state")
+
+	def _next_chunk(self):
+		raise NotImplementedError("_next_chunk")
+
+	def profile(self, min_time_secs = 3):
+		t0 = time.time()
+		length = 0
+		while True:
+			data = self.get(128 * 1024)
+			length += len(data)
+			t1 = time.time()
+			tdiff = t1 - t0
+			if tdiff > min_time_secs:
+				break
+		print("%d bytes in %.1f secs: %.1f MBytes/sec" % (length, tdiff, length / tdiff / 1e6))
+
+class HashedPRNG(PRNG):
+	"""This PRNG is *not* a CSPRNG. It's intended purely for testing
+	purposes."""
+
+	_HASHFNC = hashlib.sha512
+
+	def init_state(self, initial_state):
+		self._state = self._HASHFNC(initial_state).digest()
+
+	def _next_chunk(self):
+		result = self._state
+		self._state = self._HASHFNC(self._state).digest()
+		return result
---
+++ x509sak/tests/PRNGTests.py
@@ -0,0 +1,44 @@
+#	x509sak - The X.509 Swiss Army Knife white-hat certificate toolkit
+#	Copyright (C) 2018-2018 Johannes Bauer
+#
+#	This file is part of x509sak.
+#
+#	x509sak is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	x509sak is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with x509sak; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <JohannesBauer@gmx.de>
+
+import hashlib
+from x509sak.PRNG import HashedPRNG
+from x509sak.tests.BaseTest import BaseTest
+
+class PRNGTests(BaseTest):
+	def test_prng(self):
+		prng = HashedPRNG(b"foobar")
+		first = prng.get(128)
+		prng.reset()
+		second = prng.get(128)
+		self.assertEqual(first, second)
+
+	def test_pattern(self):
+		data = HashedPRNG(b"foobar").get(1024)
+		self.assertEqual(hashlib.md5(data).hexdigest(), "acc988f3788299f2bd603c38902ae0a7")
+		data = HashedPRNG(b"barfoo").get(1024)
+		self.assertEqual(hashlib.md5(data).hexdigest(), "fe06bbd9bf3988af7b3f2d12b20bd583")
+
+	def test_beginning(self):
+		data = HashedPRNG(b"foobar").get(1024)
+		for l in range(len(data) + 1):
+			part = HashedPRNG(b"foobar").get(l)
+			self.assertEqual(part, data[:l])
