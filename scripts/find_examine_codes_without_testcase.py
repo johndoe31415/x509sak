fa879cc0e0af58bfeafeaa47a57a9fd07fac9947
Johannes Bauer <joe@johannes-bauer.com>
2019-12-23 18:47:54 +0100

    Improved checking script
    Reworked the checking script so it will alert us to files that we
    already contain examples of something we would want to check for, but
    never did so explicitly.

--- scripts/find_examine_codes_without_testcase.py
+++ scripts/find_examine_codes_without_testcase.py
@@ -30,11 +30,23 @@ from Parallelizer import Parallelizer
 from x509sak.FriendlyArgumentParser import FriendlyArgumentParser
 
 class ResultCollector():
-	def __init__(self):
+	def __init__(self, args):
+		self._args = args
+		self._all_codes = set(code.name for code in x509sak.estimate.Judgement.JudgementCode)
 		self._errors = [ ]
-		self._codes = set()
+		self._encountered_certs = { }
 
-	def run(self, filename):
+	def _analyze(self, codes, data):
+		if isinstance(data, dict):
+			if "code" in data:
+				codes.add(data["code"])
+			for (key, value) in data.items():
+				self._analyze(codes, value)
+		elif isinstance(data, list):
+			for item in data:
+				self._analyze(codes, item)
+
+	def run(self, basefilename, filename):
 		# try to load certificate from file
 		try:
 			cert = x509sak.X509Certificate.read_pemfile(filename)
@@ -44,63 +56,64 @@ class ResultCollector():
 		print(filename, file = sys.stderr)
 		output = subprocess.check_output([ "./x509sak.py", "examine", "-f", "json", filename ], stderr = subprocess.DEVNULL)
 		json_data = json.loads(output)
-		return json_data
 
-	def _analyze(self, data):
-		if isinstance(data, dict):
-			if "code" in data:
-				self._codes.add(data["code"])
-			for (key, value) in data.items():
-				self._analyze(value)
-		elif isinstance(data, list):
-			for item in data:
-				self._analyze(item)
+		encountered_codes = set()
+		self._analyze(encountered_codes, json_data)
+		return (basefilename, encountered_codes)
+
 
 	def finished_callback(self, call_args, result):
 		if result is None:
 			return
-		(filename, ) = call_args
-		if isinstance(result, Exception):
-			print("Errored: %s (%s: %s)" % (filename, result.__class__.__name__, result), file = sys.stderr)
-			self._errors.append(filename)
-			return
-		self._analyze(result)
+		(basefilename, encounterd_codes) = call_args
 
-	def dump(self):
-		if len(self._errors) > 0:
-			print("%d files failed:" % (len(self._errors)))
-			for filename in sorted(self._errors):
-				print("    %s" % (filename))
-			print()
-		encountered = set(getattr(x509sak.estimate.Judgement.JudgementCode, name) for name in self._codes)
-		all_codes = set(x509sak.estimate.Judgement.JudgementCode)
-		missing = all_codes - encountered
-		print("%d of %d codes encountered when scanning all present test certificates. %d missing:" % (len(encountered), len(all_codes), len(missing)))
-		for item in sorted([ code.name for code in missing ]):
-			print("    %s" % (item))
+		for encountered_code in encountered_codes:
+			if encountered_code not in self._encountered_certs:
+				self._encountered_certs[encountered_code] = (basefilename, None)
+
+	def _dump_errors(self):
+		if len(self._errors) == 0:
+			return
+		print("%d files produced errors:" % (len(self._errors)))
+		for filename in sorted(self._errors):
+			print("    %s" % (filename))
 		print()
 
+	def _dump_codes(self, codes, scantype):
+		missing = self._all_codes - set(codes)
+		print("%s: %d of %d codes seen (%.1f%%), %d missing (%.1f%%):" % (scantype, len(codes), len(self._all_codes), len(codes) / len(self._all_codes) * 100, len(missing), len(missing) / len(self._all_codes) * 100))
+		for codename in sorted(missing):
+			if codename in self._all_scans:
+				(certfile, ca_certfile) = self._all_scans[codename]
+				if ca_certfile is None:
+					print("    %s (%s)" % (codename, certfile))
+				else:
+					print("    %s (%s with CA %s)" % (codename, certfile, ca_certfile))
+			else:
+				print("    %s" % (codename))
+		print()
+
+	def dump(self):
 		try:
 			with open(".examinecert_stats.json") as f:
-				stats = json.load(f)
-
-			tc_encountered = set(stats["encountered_codes"])
-			missing = all_codes - tc_encountered
-			print("%d of %d codes encountered when running testcases. %d missing:" % (len(tc_encountered), len(all_codes), len(missing)))
-			for item in sorted([ code.name for code in missing ]):
-				print("    %s" % (item))
-			print()
-
-			tc_checked = set(stats["checked_codes"])
-			missing = all_codes - tc_checked
-			print("%d of %d codes actively checked for when running testcases. %d missing:" % (len(tc_checked), len(all_codes), len(missing)))
-			for item in sorted([ code.name for code in missing ]):
-				print("    %s" % (item))
-			print()
-
+				local_stats = json.load(f)
 		except (FileNotFoundError, json.JSONDecodeError):
 			# No stat file available
-			print("No stat file available, not checking against run testcases.")
+			print("No stat file available, not checking against testcases that ran.", file = sys.stderr)
+			local_stats = False
+
+		self._all_scans = dict(self._encountered_certs)
+		if local_stats is not None:
+			self._all_scans.update(local_stats["encountered_codes"])
+			self._all_scans.update(local_stats["checked_codes"])
+
+		if self._args.scan:
+			self._dump_errors()
+			self._dump_codes(self._encountered_certs, "Local certificate scan")
+
+		self._dump_codes(local_stats["encountered_codes"], "Testcases encountered")
+		self._dump_codes(local_stats["checked_codes"], "Testcases specifically tested for")
+
 
 	def add_codes(self, codes):
 		self._codes |= set(codes)
@@ -109,7 +122,7 @@ parser = FriendlyArgumentParser(description = "Find untested result codes for th
 parser.add_argument("-s", "--scan", action = "store_true", help = "Scan all certificates in the test/ subdirectory as well to find out which codes would be emitted")
 args = parser.parse_args(sys.argv[1:])
 
-rc = ResultCollector()
+rc = ResultCollector(args)
 parallelizer = Parallelizer()
 if args.scan:
 	base_dir = "x509sak/tests/data"
@@ -119,6 +132,7 @@ if args.scan:
 				continue
 
 			full_filename = dirname + "/" + filename
-			parallelizer.run(rc.run, args = (full_filename, ), finished_callback = rc.finished_callback)
+			base_filename = full_filename[len(base_dir) : ]
+			parallelizer.run(rc.run, args = (base_filename, full_filename), finished_callback = rc.finished_callback)
 parallelizer.wait()
 rc.dump()
