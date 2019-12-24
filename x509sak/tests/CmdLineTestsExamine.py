4a5060bba91863126d6804b0000278d35ff8867e
Johannes Bauer <joe@johannes-bauer.com>
2019-12-24 08:39:50 +0100

    Also test for high Hamming weight
    We introduce a test that also tests for high Hamming weight, but only
    for X because we did not do that number crunching for Y and it's the
    same code.

--- x509sak/tests/CmdLineTestsExamine.py
+++ x509sak/tests/CmdLineTestsExamine.py
@@ -507,8 +507,11 @@ class CmdLineTestsExamine(BaseTest):
 		# when the curve is known, which is encoded in the CA's public key.
 		self._test_examine_x509test_resultcode("certs/constructed/ecdsa_sig_s_bitbias.pem", "ECDSA_Signature_S_BitBias", parent_certname = "certs/ok/johannes-bauer.com.pem")
 
-	def test_constructed_pubkey_bitbias_x(self):
-		self._test_examine_x509test_resultcode("certs/constructed/ecc_pubkey_x_bitbias.pem", "ECC_Pubkey_X_BitBias", expect_absent = "ECC_Pubkey_Y_BitBias")
+	def test_constructed_pubkey_bitbias_x_low_hweight(self):
+		self._test_examine_x509test_resultcode("certs/constructed/ecc_pubkey_x_bitbias1.pem", "ECC_Pubkey_X_BitBias", expect_absent = "ECC_Pubkey_Y_BitBias")
+
+	def test_constructed_pubkey_bitbias_x_high_hweight(self):
+		self._test_examine_x509test_resultcode("certs/constructed/ecc_pubkey_x_bitbias2.pem", "ECC_Pubkey_X_BitBias", expect_absent = "ECC_Pubkey_Y_BitBias")
 
 	def test_constructed_pubkey_bitbias_y(self):
 		self._test_examine_x509test_resultcode("certs/constructed/ecc_pubkey_y_bitbias.pem", "ECC_Pubkey_Y_BitBias", expect_absent = "ECC_Pubkey_X_BitBias")
