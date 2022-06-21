5f323493d945e842f6da4e6dd5912b5a44ca1c4a
Johannes Bauer <joe@johannes-bauer.com>
2022-06-21 09:37:03 +0200

    Release v0.0.4
    Only fix is that installation now should work without having
    pyasn1_modules present in the tree.

--- x509sak/__init__.py
+++ x509sak/__init__.py
@@ -1,5 +1,5 @@
 #	x509sak - The X.509 Swiss Army Knife white-hat certificate toolkit
-#	Copyright (C) 2018-2021 Johannes Bauer
+#	Copyright (C) 2018-2022 Johannes Bauer
 #
 #	This file is part of x509sak.
 #
@@ -24,4 +24,4 @@ from .X509Certificate import X509Certificate
 from .CertificatePool import CertificatePool
 from .CAManager import CAManager
 
-VERSION = "0.0.4rc0"
+VERSION = "0.0.4"
